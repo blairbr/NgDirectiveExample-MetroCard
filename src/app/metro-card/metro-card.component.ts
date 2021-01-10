08f3ac000cfa104f46620a424dba2e05b7c34e0f
Blair Brown <blair.j.brown@gmail.com>
2021-01-10 09:29:59 -0500

    added currency pipe

--- src/app/metro-card/metro-card.component.ts
+++ src/app/metro-card/metro-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface MetroCard {
   owner: string;
   balance: number;
-  active: boolean;
+  isActive: boolean;
 }
 
 @Component({
@@ -13,7 +13,14 @@ interface MetroCard {
 })
 
 export class MetroCardComponent implements OnInit {
-  public metroCards : MetroCard[] = [{owner: "Blair", balance: 20, active: false}, {owner: 'Sputnik', balance: 0, active: true}, {owner: 'Freddie', balance: 3.5, active: true}, {owner:'Luca', balance: -1.90, active: true}];
+
+  
+  
+  public metroCards : MetroCard[] = [{owner: "Blair", balance: 20, isActive: false}, {owner: 'Sputnik', balance: 0, isActive: true}, {owner: 'Freddie', balance: 3.5, isActive: true}, {owner:'Luca', balance: -1.90, isActive: true}];
+  // stylesList: object = {
+  //   'font-style':  isActive    ? 'italic' : 'normal',
+  //   'font-weight': balance    ? 'bold'   : 'normal',
+  // }
 
   constructor() { }
 
