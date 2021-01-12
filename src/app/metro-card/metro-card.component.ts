a9fa0c7b567fc3bee871b191d86381b19d3e38b5
Blair Brown <blair.j.brown@gmail.com>
2021-01-11 23:54:44 -0500

    added ngStyle of line-through

--- src/app/metro-card/metro-card.component.ts
+++ src/app/metro-card/metro-card.component.ts
@@ -14,9 +14,13 @@ interface MetroCard {
 
 export class MetroCardComponent implements OnInit {
 
-  
-  
-  public metroCards : MetroCard[] = [{owner: "Blair", balance: 20, isActive: false}, {owner: 'Sputnik', balance: 0, isActive: true}, {owner: 'Freddie', balance: 3.5, isActive: true}, {owner:'Luca', balance: -1.90, isActive: true}];
+  public metroCards : MetroCard[] = [
+    { owner: "Blair", balance: 20, isActive: false },
+    { owner: 'Sputnik', balance: 0, isActive: true }, 
+    { owner: 'Freddie', balance: 3.5, isActive: true }, 
+    {owner:'Luca', balance: -1.90, isActive: true}
+  ];
+
   // stylesList: object = {
   //   'font-style':  isActive    ? 'italic' : 'normal',
   //   'font-weight': balance    ? 'bold'   : 'normal',
